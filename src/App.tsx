be16a7406d8114db5d833df9ef9fdf147618b920
Erick Carvalho <erick.carvalho@teamsoft.com.br>
2025-03-01 18:09:37 -0300

    Testando Husky 2

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const queryClient = new QueryClient({
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={}>
       <RouterProvider router={router} />
     </QueryClientProvider>
   );
