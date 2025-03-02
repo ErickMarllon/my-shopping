3e61f350dddd008cd8282b51ff20984d02c77147
Erick Carvalho <erick.carvalho@teamsoft.com.br>
2025-03-02 03:37:18 -0300

    testing pre-commit code

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const queryClient = new QueryClient({
 
 function App() {
   return (
-    <QueryClientProvider client={}>
+    <QueryClientProvider>
       <RouterProvider router={router} />
     </QueryClientProvider>
   );
