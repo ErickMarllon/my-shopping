e34bae624be57b9c0af28bc5289100f1c7200260
Erick Carvalho <erick.carvalho@teamsoft.com.br>
2025-03-12 12:47:08 -0300

    feat(types): add custom MUI typings for PaperProps, ColorRange, and Breakpoints
    - Extended `PaperPropsVariantOverrides` to include `highlighted` variant
    - Defined custom `ColorRange` interface for extended color palette
    - Added custom breakpoints (`mb`, `mobile`) to `BreakpointOverrides`

---
+++ src/types/mui.d.ts
@@ -0,0 +1,30 @@
+declare module "@mui/material/Paper" {
+  interface PaperPropsVariantOverrides {
+    highlighted: true;
+  }
+}
+declare module "@mui/material/styles/createPalette" {
+  interface ColorRange {
+    50: string;
+    100: string;
+    200: string;
+    300: string;
+    400: string;
+    500: string;
+    600: string;
+    700: string;
+    800: string;
+    900: string;
+  }
+}
+declare module "@mui/material/styles" {
+  interface BreakpointOverrides {
+    xs: true;
+    sm: true;
+    md: true;
+    lg: true;
+    xl: true;
+    mb: true;
+    mobile: true;
+  }
+}
